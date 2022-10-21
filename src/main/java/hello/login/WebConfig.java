2fe7b221a9c7d2221968cf5e56dd3d8089eee27c
kk210001 <kk210001@naver.com>
2022-10-21 19:37:39 +0900

    webConfig에 인터셉터 등록 order(1) 적용 순서 첫번째 addPathPatterns("/**") 적용할 경로 하위폴더 까지 전부  .excludePathPatterns("/css/**","/*.ico","/error") 제외처리 되는 경로

--- src/main/java/hello/login/WebConfig.java
+++ src/main/java/hello/login/WebConfig.java
@@ -3,15 +3,18 @@ package hello.login;
 
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
+import hello.login.web.interceptor.LoginInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
 
 @Configuration
-public class WebConfig {
-    @Bean
+public class WebConfig implements WebMvcConfigurer {
+   // @Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
         filterFilterRegistrationBean.setFilter(new LogFilter());
@@ -29,5 +32,12 @@ public class WebConfig {
         filterFilterRegistrationBean.addUrlPatterns("/*");
 
         return filterFilterRegistrationBean;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor()).order(1).addPathPatterns("/**")
+                .excludePathPatterns("/css/**","/*.ico","/error");
+
     }
 }
